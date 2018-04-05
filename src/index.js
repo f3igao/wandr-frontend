86b520a7d9e163efae06f8185ca42b3e5304de8e
feihafferkamp <fei.hafferkamp@gmail.com>
2018-04-05 17:22:41 -0400

    add reducers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+	<Router>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</Router>,
+	document.getElementById('root')
+);
+
 registerServiceWorker();
