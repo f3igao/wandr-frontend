1d73e5b646fff307f8c81e7f7e15433b7cc33639
f3igao <f3igao@icloud.com>
2018-04-10 09:51:57 -0400

    finish CRUD user trips

--- src/reducers/tripReducer.js
+++ src/reducers/tripReducer.js
@@ -10,6 +10,12 @@ export default function(state = defaultState, action) {
 			return { ...state, userTrips: [...state.userTrips, action.newTrip] };
 		case 'EDIT_TRIP':
 			return { ...state, trip: action.editedTrip };
+		case 'DELETE_TRIP':
+			const index = state.userTrips.findIndex(t => t.id === action.userTripId);
+			const newUserTrips = state.userTrips
+				.slice(0, index)
+				.concat(state.userTrips.slice(index));
+			return { ...state, userTrips: newUserTrips, trip: {} };
 		default:
 			return state;
 	}
