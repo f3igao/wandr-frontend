e30f9bac9c69804bbbb4bb0b51f480f4558d514a
feihafferkamp <fei.hafferkamp@gmail.com>
2018-04-11 17:38:20 -0400

    add initial polyline

--- src/trips/TripMap.js
+++ src/trips/TripMap.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
+import {
+	withScriptjs,
+	withGoogleMap,
+	GoogleMap,
+	Polyline
+} from 'react-google-maps';
 // import { MarkerClusterer } from 'react-google-maps/lib/components/addons/MarkerClusterer';
 import ActivityMarker from '../activities/ActivityMarker';
 
 const TripMap = withScriptjs(
 	withGoogleMap(props => {
-		console.log(props.activities);
-		const displayActivityMarkers = props.activities.map((a, i) => (
+		const activityMarkers = props.activities.map((a, i) => (
 			<ActivityMarker activity={a} key={i} />
 		));
 
+		const polylinePath = props.activities.map(a => ({
+			lat: Number(a.lat),
+			lng: Number(a.lng)
+		}));
+
 		return (
-			<GoogleMap defaultZoom={2} defaultCenter={{ lat: 0, lng: 0 }}>
-				{displayActivityMarkers}
+			<GoogleMap defaultZoom={3} defaultCenter={{ lat: 0, lng: 0 }}>
+				{activityMarkers}
+				<Polyline path={polylinePath} />
 			</GoogleMap>
 		);
 	})
