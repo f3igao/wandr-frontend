2ad15e70a9ca5aa72f12e1c77522872d50bca269
f3igao <f3igao@icloud.com>
2018-04-17 11:16:11 -0400

    start direct messages

--- src/chatroom/ChatroomList.js
+++ src/chatroom/ChatroomList.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 
-export default class ChatroomList extends Component {
-	render() {
-		return this.props.chatrooms.map(chatroom => {
-			return (
-				<li
-					key={chatroom.id}
-					onClick={() => this.props.selectRoom(chatroom.id)}>
-					{chatroom.name}
-				</li>
-			);
-		});
-	}
-}
+const ChatroomList = props => {
+	return props.friends.map(f => (
+		<button key={f.id} onClick={() => props.selectRoom(f.id)}>
+			{f.firstname}
+		</button>
+	));
+};
+
+export default ChatroomList;
--- src/friends/FriendsContainer.js
+++ src/friends/FriendsContainer.js
@@ -3,9 +3,10 @@ import { connect } from 'react-redux';
 import withAuth from '../app/withAuth';
 import Navbar from '../app/Navbar';
 import UserCard from './UserCard';
-import ChatroomContainer from '../chatroom/ChatroomContainer';
 import { withRouter } from 'react-router-dom';
 import { fetchUsers } from '../actions/friendActions';
+import ChatroomContainer from '../chatroom/ChatroomContainer';
+import { Grid } from 'semantic-ui-react';
 
 class FriendsContainer extends Component {
 	state = { loaded: false };
@@ -25,33 +26,39 @@ class FriendsContainer extends Component {
 			<div>
 				<Navbar history={this.props.history} />
 				{this.state.loaded ? (
-					<div>
-						<h1>Friends</h1>
-						{this.props.friends.map((u, i) => (
-							<UserCard key={i} user={u} status={'friends'} />
-						))}
-						<h3>You've sent requests to...</h3>
-						{this.props.requestedFriends.map((u, i) => (
-							<UserCard
-								key={i}
-								user={u}
-								status={'requested'}
-								friendshipId={u.friendshipId}
-							/>
-						))}
-						<h3>You have pending requests from...</h3>
-						{this.props.pendingFriends.map((u, i) => (
-							<UserCard key={i} user={u} status={'pending'} />
-						))}
-						<h3>Add more friends</h3>
-						{this.props.strangers.map((u, i) => (
-							<UserCard key={i} user={u} status={'strangers'} />
-						))}
-					</div>
+					<Grid columns={2} divided>
+						<Grid.Column width={11}>
+							<div>
+								<h1>Friends</h1>
+								{this.props.friends.map((u, i) => (
+									<UserCard key={i} user={u} status={'friends'} />
+								))}
+								<h3>You've sent requests to...</h3>
+								{this.props.requestedFriends.map((u, i) => (
+									<UserCard
+										key={i}
+										user={u}
+										status={'requested'}
+										friendshipId={u.friendshipId}
+									/>
+								))}
+								<h3>You have pending requests from...</h3>
+								{this.props.pendingFriends.map((u, i) => (
+									<UserCard key={i} user={u} status={'pending'} />
+								))}
+								<h3>Add more friends</h3>
+								{this.props.strangers.map((u, i) => (
+									<UserCard key={i} user={u} status={'strangers'} />
+								))}
+							</div>
+						</Grid.Column>
+						<Grid.Column width={5}>
+							<ChatroomContainer friends={this.props.friends} />
+						</Grid.Column>
+					</Grid>
 				) : (
 					'Loading friends info...'
 				)}
-				<ChatroomContainer />
 			</div>
 		);
 	}
